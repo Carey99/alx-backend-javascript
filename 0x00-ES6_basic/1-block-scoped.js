63f9817abbd28ce2c56e7d6dcacf4884ff61b2cb
Carey99 <edwinakidah1@gmail.com>
2024-06-24 23:33:06 +0300

    variable hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    var task = false;
+    var task2 = true;
   
     if (trueOrFalse) {
       let task = true;
       let task2 = false;
-      console.log('Inside if:', task, task2);
+
     }
     
-    console.log('Outside if:', task, task2);
     return [task, task2];
   }
