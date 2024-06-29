49b92c3a9aabf43a6a0cc63d3e9df86e1d0562c4
Carey99 <edwinakidah1@gmail.com>
2024-06-29 05:46:54 +0300

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(value) {
+        this._amount = value;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(value) {
+        this._currency = value;
+    }
+    displayFullPrice() {
+        return `${this.amount} - ${this.currency.displayFullCurrency()}`;
+    }
+
+    static convertprice(amount, conversionRate) {
+        return new Pricing(amount * conversionRate, Currency.USD);
+    }
+}
