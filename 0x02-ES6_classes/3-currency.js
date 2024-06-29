e0327bfebc2e97e865809b4f923183a683e98fb8
Carey99 <edwinakidah1@gmail.com>
2024-06-29 05:35:10 +0300

    Currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,15 +1,12 @@
 export default class Currency {
     constructor(code, name) {
-        this.code = code;
-        this.name = name;
+        this._code = code;
+        this._name = name;
     }
     get code() {
         return this._code;
     }
     set code(value) {
-        if (value.length !== 3) {
-        throw new Error('Currency code must be 3 characters.');
-        }
         this._code = value;
     }
     displayFullCurrency() {
