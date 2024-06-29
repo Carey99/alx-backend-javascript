3a8190bf0207b291b0853d807daf69e458b679df
Carey99 <edwinakidah1@gmail.com>
2024-06-29 04:55:45 +0300

    Error number 3

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,5 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      if (typeof maxStudentsSize !== 'number') {
-        throw new TypeError('maxStudentsSize must be a number');
-      }
-      this._maxStudentsSize = maxStudentsSize;
+    constructor(maxStudentsSize){
+        this._maxStudentsSize = maxStudentsSize;
     }
-  }
+}
