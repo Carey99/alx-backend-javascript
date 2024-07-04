5fff95831f56b323f21bf757b28521a07b377d6c
Carey99 <edwinakidah1@gmail.com>
2024-07-04 05:07:10 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,4 +19,25 @@ interface PrintTeacherFunction {
 function printTeacher(teacher: PrintTeacherFunction): string {
     return `${teacher.firstName.charAt(0)}. ${teacher.lastName}`;
 }
-    
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
