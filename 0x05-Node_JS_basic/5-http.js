5b349971b04c4d6a63a9654a3e6c1bf30f545f6f
Carey99 <edwinakidah1@gmail.com>
2024-08-21 00:22:09 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,31 @@
 const http = require('http');
 const fs = require('fs');
-const { countStudents } = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    const { method, url } = req;
+  const { method, url } = req;
 
-    if (url === '/') {
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (url === '/students') {
+    const databasePath = 'database.csv';
+    fs.readFile(databasePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error\n');
+      } else {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Holberton School!\n');
-    } else if (url === '/students') {
-        const databasePath = process.argv[2];
-        fs.readFile(databasePath, 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error\n');
-            } else {
-                const students = countStudents(data);
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(students);
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found\n');
-    }
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
 });
 
 app.listen(1245, () => {
-    console.log('Server running on http://localhost:1245');
+  console.log('Server running on http://localhost:1245');
 });
 
 module.exports = app;
